e9691a4f88190fc5c983c58675c2eac8686803e9
Chef <Chef@DEV-PC>
2022-12-08 20:41:50 +0100

    Initial Code Commit
    Added the Azure Functions Blob-Trigger template project files.

---
+++ excel-file-content-extractor/src/ExcelFileContentExtractor.FunctionApp/Functions/ExcelFileContentExtractorFuncApp.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace ExcelFileContentExtractor.FunctionApp
+{
+    public class ExcelFileContentExtractorFuncApp
+    {
+        [FunctionName("excel-file-content-extractor-func-app")]
+        public void Run([BlobTrigger("mis-test/{name}", Connection = "blobCon")]Stream myBlob, string name, ILogger log, Uri uri)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes \n Uri: {uri.AbsolutePath}");
+        }
+    }
+}
