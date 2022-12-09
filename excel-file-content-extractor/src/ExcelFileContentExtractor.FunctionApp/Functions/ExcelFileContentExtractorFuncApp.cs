5381f6881fd3a17fdb505e64049966ef77063b66
Daniel-Zahn <120014365+Daniel-Zahn@users.noreply.github.com>
2022-12-09 17:21:50 +0100

    Update ExcelFileContentExtractorFuncApp.cs

--- excel-file-content-extractor/src/ExcelFileContentExtractor.FunctionApp/Functions/ExcelFileContentExtractorFuncApp.cs
+++ excel-file-content-extractor/src/ExcelFileContentExtractor.FunctionApp/Functions/ExcelFileContentExtractorFuncApp.cs
@@ -3,15 +3,11 @@ using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
-using log4net;
-using log4net.Config;
-using log4net.Repository;
 
 namespace ExcelFileContentExtractor.FunctionApp
 {
     public class ExcelFileContentExtractorFuncApp
     {
-        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         
         [FunctionName("excel-file-content-extractor-func-app")]
         public void Run([BlobTrigger("mis-test/{name}", Connection = "blobCon")]Stream myBlob, string name, ILogger log, Uri uri)
@@ -20,9 +16,3 @@ namespace ExcelFileContentExtractor.FunctionApp
         }
     }
 }
-
-// Azure.Storage.Blobs(12.14.1)
-// log4net (2.0.9)
-// NHibernate (5.2.7)
-// NPOI (2.5.6)
-// System.Data.SqlClient (4.8.3)
